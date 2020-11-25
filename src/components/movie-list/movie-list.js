b3227d543897e1feda3661bd5b8f8b7b45d93bea
Ramil Baybulatov <ramilbajbulatov1488@gmail.com>
2020-11-25 18:48:31 +0500

    first commit

---
+++ src/components/movie-list/movie-list.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Movie from '../movie';
+
+import './movie-list.css';
+
+const MovieList = ({movies}) => {
+	console.log(movies);
+	const cards = movies.map((elem) => {
+		return(
+			<li className="card" key={elem.id}>
+				<Movie {...elem}  />	
+			</li>
+		)
+	})
+	return (
+		<ul className="main__list">
+			{cards}
+		</ul>
+	)
+}
+
+MovieList.defaultProps = {
+	movies: [{
+		adult: false,
+		backdrop_path: "/cXnlomnqXZF8Lvcev5UAJgfx4gt.jpg",
+		genre_ids: (4) [12, 16, 35, 10751],
+		id: 615774,
+		original_language: "en",
+		original_title: "Scooby-Doo! Return to Zombie Island",
+		overview: "Scooby-Doo and his pals win an all-expense paid vacation and embark on a trip of a lifetime to a tropical paradise. Their destination however, turns out to be Zombie Island. As soon as they arrive, they realize the place looks strangely familiar and is reminiscent of a trip they took years ago, in which they became wrapped up in a mystery involving zombies. The gang soon learns that their trip to paradise comes with a price when the zombies re-emerge and attack their hotel. Will Scooby-Doo and the Mystery Inc. gang finally solve the mystery behind Zombie Island?",
+		popularity: 121.11,
+		poster_path: "/vd0oIoWhEm8o7EpS0kV7RaV0Flf.jpg",
+		release_date: "2019-10-01",
+		title: "Scooby-Doo! Return to Zombie Island",
+		video: false,
+		vote_average: 7.1,
+		vote_count: 318,
+	}]
+} 
+
+MovieList.propTypes = {
+	movies: PropTypes.arrayOf(PropTypes.object)
+}
+
+
+export default MovieList;
---
+++ src/components/movie/movie.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+
+import SwapiServices from '../swapi-services/swapi-services';
+
+import './movie.css'
+
+export default class Movie extends Component {
+	trimParagraph = (overview) => {
+		if(overview.length > 200) {
+			const trimOveriew = overview.slice(0, 200);
+			const lastSpace = trimOveriew.lastIndexOf(' ');
+			return `${trimOveriew.slice(0, lastSpace)}...`
+		} return overview
+	}
+
+	render() {
+		const { overview, poster_path, title, release_date } = this.props;
+		const date = new Date(release_date);
+		const dateArr = String(date).split(' ', 4);
+		const trimOveriew = this.trimParagraph(overview);
+		return(
+				<div className="card__container">
+					
+						<img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} className="card__img" />
+
+					<div>
+						<h3 className="card__title">{title}</h3>
+						<span className="card__date">{`${dateArr[1]} ${dateArr[2]}, ${dateArr[3]}`}</span>
+						<div>
+							<button></button>
+							<button></button>
+						</div>
+						<p className="card__paragraph">{trimOveriew}</p>
+					</div>
+				</div>
+		)
+	}
+}
